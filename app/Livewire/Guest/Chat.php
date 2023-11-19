5b3cd20e508d6f42094ab151c293ce2c303cf375
abdulrahmansbq <abdulrahmansbq@gmail.com>
2023-11-19 13:57:04 +0300

    Update the logic in chat to include the whole process

--- app/Livewire/Guest/Chat.php
+++ app/Livewire/Guest/Chat.php
@@ -2,30 +2,76 @@
 
 namespace App\Livewire\Guest;
 
+use App\Models\Chat as ChatModel;
+use App\Models\Session;
+use App\Services\EmbeddingService;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Chat extends Component
 {
+    #[Rule('required|min:3|max:1024')]
     public $question;
+
+    public $chatUuid;
+
+    public function mount($chatUuid = null)
+    {
+        $this->chatUuid = $chatUuid;
+    }
 
     public function render()
     {
-        return view('livewire.guest.chat')->layout('components.layouts.chat');
-    }
-
-    public function submit()
-    {
-        $this->validate([
-            'question' => 'required|min:3'
-        ]);
-
-        $this->emit('chatMessageReceived', $this->question);
-
-        $this->question = '';
+        $chat = ChatModel::with('messages')->find($this->chatUuid);
+        return view('livewire.guest.chat', compact('chat'))->layout('components.layouts.chat');
     }
 
     public function sendMessage()
     {
-        sleep(4);
+        $this->validate();
+
+        $question = $this->question;
+
+        $this->reset('question');
+
+
+        $sessionId = session()->getId();
+
+        $chat = ChatModel::find($this->chatUuid);
+
+        abort_if($chat && $chat->session_id !== $sessionId, 403);
+
+        $session = Session::find($sessionId);
+
+        if (!$chat) {
+            $chat = $session->chats()->create();
+        }
+
+        $message = $chat->messages()->create([
+            'content' => $question,
+            'session_id' => $sessionId,
+        ]);
+
+
+        if (!$this->chatUuid) {
+            $this->redirect(route('chat.show', $chat->uuid), navigate: true);
+        }
+
+        $embeddingService = (new EmbeddingService);
+
+        $embeddings = $embeddingService->getOpenAIEmbedding($question, $message);
+
+        $wordIdInSiwar = $embeddingService->getWordIdFromVectorDB($embeddings, $message);
+
+        $siwarDetails = $embeddingService->getSiwarDetails($wordIdInSiwar, $message);
+
+        $answer = $embeddingService->getGptAnswer($siwarDetails, $message);
+
+        $message = $chat->messages()->create([
+            'content' => $answer,
+            'is_bot' => true,
+        ]);
     }
+
+
 }
