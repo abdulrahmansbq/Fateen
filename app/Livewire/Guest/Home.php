a15fd207fc1473e4ce307a17eba9f82da4b4e928
abdulrahmansbq <abdulrahmansbq@gmail.com>
2023-11-19 10:30:38 +0300

    Add the home page as a livewire component

---
+++ app/Livewire/Guest/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Guest;
+
+use Livewire\Component;
+
+class Home extends Component
+{
+    public function render()
+    {
+        return view('livewire.guest.home');
+    }
+}
