1a9293158e6330099b481800f4cba1ab0a1e68b7
abdulrahmansbq <abdulrahmansbq@gmail.com>
2023-11-19 10:32:17 +0300

    Add the chat livewire view

---
+++ resources/views/livewire/guest/chat.blade.php
@@ -0,0 +1,58 @@
+<div>
+    <div class="chat-header" wire:ignore>
+        <div class="d-flex align-items-center gap-2">
+            <button class="navbar-toggler d-md-none d-block" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#mainnavbarNav" aria-controls="mainnavbarNav" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <i class="iconsax" data-icon="text-align-justify"></i>
+            </button>
+            <a href="index.html" class="logo-icon d-flex d-md-none">
+                <img src="{{ asset('assets/svg/logo-icon.svg') }}" class="img-fluid">
+            </a>
+            <h3 id="targetDiv">قم بكتابة استفسارك</h3>
+        </div>
+        <div class="header-option">
+            <a class="del-btn" data-cursor="pointer" href="chat.html"><i class="iconsax"
+                                                                         data-icon="trash"></i></a>
+            <a class="premium-btn" id="subscriptionBtn" data-cursor="pointer" href="chat.html#!"><i
+                    class="iconsax" data-icon="crown-2"></i> Get
+                <span>premium</span></a>
+        </div>
+    </div>
+    <div class="main-chat">
+        <div class="no-chat d-none">
+            <div>
+                <img src="{{ asset('assets/svg/no-chat.svg') }}" class="img-fluid" alt="">
+                <h3>لا توجد أي استفسارات</h3>
+            </div>
+        </div>
+        <div class="msger-chat">
+            <div class="msg right-msg" >
+                <div class="msg-bubble" wire:loading style="display: none;">
+                    <div class="msg-text">
+                        <div class="spinner-grow" style="width: 1rem; height: 1rem;" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                        <div class="spinner-grow" style="width: 1rem; height: 1rem;" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                        <div class="spinner-grow" style="width: 1rem; height: 1rem;" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="msger-inputarea">
+            <button class="navbar-toggler d-lg-none d-block msger-send-btn" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <i class="iconsax" data-icon="text-align-justify"></i>
+            </button>
+            <input type="text" class="msger-input" placeholder="قم بكتابة الإستفسار هنا..." wire:keydown.enter="sendMessage()"
+                   wire:model="question" />
+            <button type="submit" class="msger-send-btn" id="sendBtn" wire:click="sendMessage()"><i class="iconsax" data-icon="send-1" wire:ignore ></i></button>
+        </div>
+    </div>
+</div>
