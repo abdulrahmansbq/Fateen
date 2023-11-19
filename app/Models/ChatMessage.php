d3cc93f0b90e6cf0605dbf110360e5ad9a1028f0
abdulrahmansbq <abdulrahmansbq@gmail.com>
2023-11-19 13:55:32 +0300

    Update chat message to cast the enum

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\Status\MessageEmbeddingStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,10 @@ class ChatMessage extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    protected $casts = [
+        'embedding_status' => MessageEmbeddingStatus::class
+    ];
 
     /**
      * Get the chat of a specific ChatMessage
