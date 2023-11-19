2c711ec307940fc80460d0630408c2b8ea8ec2f2
abdulrahmansbq <abdulrahmansbq@gmail.com>
2023-11-19 10:32:55 +0300

    Create the chat model

---
+++ app/Models/Chat.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Chat extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $guarded = ['uuid'];
+
+    /**
+     * Get all the messages of a specific Chat
+     */
+    public function messages(): HasMany
+    {
+        return $this->hasMany(ChatMessage::class);
+    }
+
+    /**
+     * Get the session of a specific Chat
+     */
+    public function session(): BelongsTo
+    {
+        return $this->belongsTo(Session::class);
+    }
+}
