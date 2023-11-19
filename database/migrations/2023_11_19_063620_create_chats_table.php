f3d2a9c21adac800789fac9cf0851bcc068d53b7
abdulrahmansbq <abdulrahmansbq@gmail.com>
2023-11-19 10:29:40 +0300

    Create chats table

---
+++ database/migrations/2023_11_19_063620_create_chats_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->uuid();
+            $table->string('session_id');
+            $table->foreign('session_id')
+                ->references('id')
+                ->on('sessions')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
